72a896af3c914988fc059db8a55862e0500f313e
Amelia <amelia.riding@Sofology.co.uk>
2021-02-04 10:43:04 +0000

    Added Javascript, score works, the images don't

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const diceScore = document.querySelector("#score");
-const diceImg = document.querySelector("#dice");
-const diceRoll = document.querySelector("#button")
+let diceImg = document.querySelector("#img");
+let diceRoll = document.querySelector("#rolldice")
+let diceArr =[];
+
+function rollDice() {
+    let rollResult = Math.floor(Math.random() * 6) + 1;
+    if (rollResult == 1) {
+        diceScore.textContent ="You Lose!"
+    } else {
+        diceScore.textContent = "Score:  " + rollResult;
+        diceArr.push(rollResult);
+        console.log(diceArr);
+    }
+      console.log(rollResult);
+      rollImage.setAttribute("src", "img/dice" + rollResult + ".png");
+    } 
+    diceRoll.addEventListener("click", () => {
+        rollDice();
+    });
