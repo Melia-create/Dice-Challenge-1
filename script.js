2d81faa504e084c180327bd809dad617e7745c6d
Amelia <amelia.riding@Sofology.co.uk>
2021-02-04 12:17:24 +0000

    Added Javascript and index.html

---
+++ script.js
@@ -0,0 +1,35 @@
+const diceScore = document.querySelector("#score");
+let diceImg = document.querySelector("#result");
+let diceRoll = document.querySelector("#rolldice")
+let header = document.querySelector("#header");
+let diceArr =[];
+
+function rollDice() {
+    let rollResult = Math.floor(Math.random() * 6) + 1;
+    if (rollResult == 1) {
+        diceScore.textContent ="You Lose!"
+        diceArr = [];
+        diceRoll.textContent = "Start Again!"
+    } else {
+        diceScore.textContent = "Score:  " + rollResult;
+        diceArr.push(rollResult);
+        let sum = diceArr.reduce((a, b) => {
+            return a + b;
+        
+    });
+    diceScore.textContent = "Score: " + sum
+    if (sum >= 20){
+        diceScore.textContent = "Score:  " + sum + " - You Win!"
+        diceRoll.textContent = "Start Again!"
+        diceArr=[]
+    }
+    }
+
+      diceImg.innerHTML ='<img src = "img/dice' + rollResult + '.png">';
+    } 
+    diceRoll.addEventListener("click", () => {
+        if(diceRoll.textContent = "Start Again!"){
+            diceRoll.textContent = "Roll"
+        }
+        rollDice();
+});
