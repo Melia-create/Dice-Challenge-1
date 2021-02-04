47a4ba2548d1a78f0b06171b6c86f35fe358db61
Amelia <amelia.riding@Sofology.co.uk>
2021-02-04 11:27:42 +0000

    Game now works, need to tidy up the layout

--- script.js
+++ script.js
@@ -7,13 +7,23 @@ function rollDice() {
     let rollResult = Math.floor(Math.random() * 6) + 1;
     if (rollResult == 1) {
         diceScore.textContent ="You Lose!"
+        diceArr = [];
     } else {
         diceScore.textContent = "Score:  " + rollResult;
         diceArr.push(rollResult);
-        diceArr = [];
+        let sum = diceArr.reduce((a, b) => {
+            return a + b;
+        
+    });
+    diceScore.textContent = "Score: " + sum
+    if (sum >= 20){
+        diceScore.textContent = "Score:  " + sum + "You Win!"
+        diceArr=[]
     }
+    }
+
       diceImg.innerHTML ='<img src = "img/dice' + rollResult + '.png">';
     } 
     diceRoll.addEventListener("click", () => {
         rollDice();
-    });
+});
